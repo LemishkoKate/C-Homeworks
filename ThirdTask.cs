7a35afebb67937e3d2034d3a08e9090cfe6eeb39
Kate Lemishko <87851510+LemishkoKate@users.noreply.github.com>
2025-10-07 22:49:12 +0300

    Create ThirdTask.cs

---
+++ ThirdTask.cs
@@ -0,0 +1,36 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EvenNumbersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            int originalNumber = a;
+            int sum = 0;
+
+            // length - length of a
+            int digits = a.ToString().Length;
+
+            while (a > 0)
+            {
+                int digit = a % 10;
+                sum += (int)Math.Pow(digit, digits);
+                a /= 10;
+            }
+
+            if (sum == originalNumber)
+            {
+                Console.WriteLine($"{originalNumber} is an Armstrong number");
+            }
+            else if (sum != originalNumber) 
+            {
+                Console.WriteLine($"{originalNumber} is not an Armstrong number");
+            }
+
+        }
+    }
+}
