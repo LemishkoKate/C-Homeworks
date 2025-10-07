4f0002e69889d1c499345ee63bc1d52138c328ec
Kate Lemishko <87851510+LemishkoKate@users.noreply.github.com>
2025-10-07 22:39:17 +0300

    Update SecondTask.cs

--- SecondTask.cs
+++ SecondTask.cs
@@ -1,10 +1,33 @@
-﻿<Project Sdk="Microsoft.NET.Sdk">
+namespace EvenNumbersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
 
-  <PropertyGroup>
-    <OutputType>Exe</OutputType>
-    <TargetFramework>net8.0</TargetFramework>
-    <ImplicitUsings>enable</ImplicitUsings>
-    <Nullable>enable</Nullable>
-  </PropertyGroup>
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            // parse - conversion of text into a number of type int
 
-</Project>
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            Console.WriteLine($"\nEven numbers between {a} and {b}:");
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " "); 
+                }
+            }
+
+        }
+    }
+}
