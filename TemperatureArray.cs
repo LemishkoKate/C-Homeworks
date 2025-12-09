e91e0fa7fc4b37fd60d82101b5206a8ecea0b35f
Kate Lemishko <87851510+LemishkoKate@users.noreply.github.com>
2025-12-09 17:48:19 +0200

    Update TemperatureArray.cs

--- TemperatureArray.cs
+++ TemperatureArray.cs
@@ -6,23 +6,51 @@ using System.Threading.Tasks;
 
 namespace OperatorsOverflow
 {
-    internal class TemperatureArray
+    public class TemperatureArray
     {
-        private double[] temps = new double[7];
+        public double  Monday {  get; set; }
+        public double  Tuesday {  get; set; }
+        public double  Wednesday {  get; set; }
+        public double  Thursday {  get; set; }
+        public double  Friday {  get; set; }
+        public double  Saturday {  get; set; }
+        public double  Sunday {  get; set; }
 
         public double this[int index]
         {
-            get { return temps[index]; }
-            set { temps[index] = value; }
+            get 
+            {
+                return index switch
+                {
+                    0 => Monday,
+                    1 => Tuesday,
+                    2 => Wednesday,
+                    3 => Thursday,
+                    4 => Friday,
+                    5 => Saturday,
+                    6 => Sunday,
+                    7 => this[index]
+                };
+            }
+
+            set
+            {
+                switch (index)
+                {
+                    case 0: Monday = value; break;
+                    case 1: Tuesday = value; break;
+                    case 2: Wednesday = value; break;
+                    case 3: Thursday = value; break;
+                    case 4: Friday = value; break;
+                    case 5: Saturday = value; break;
+                    case 6: Sunday = value; break;
+                }
+            }
         }
 
         public double GetAverage()
         {
-            double sum = 0;
-            for (int i = 0; i < temps.Length; i++)
-                sum += temps[i];
-
-            return sum / temps.Length;
+            return (Monday + Tuesday + Wednesday + Thursday + Friday + Saturday + Sunday) / 7.0;
         }
     }
 }
